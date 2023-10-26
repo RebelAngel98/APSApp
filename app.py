95cfdc9cbb41fe0744df3ad7ed2cf86f25b60a7f
RebelAngel98 <katlenno@uat.edu>
2023-10-26 12:33:29 -0700

    text-styles added

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
-import os
-from openpyxl import Workbook
 
 #DO NOT TOUCH! This is bringing the 'static' folder into the app. You can completely crash the app if you change it
 app = Flask(__name__, static_folder='static')
